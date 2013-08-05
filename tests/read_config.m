7fe38fd6b9cf7a0ca7abadd0381f055f153a1552
Glaser <glaser@deep-banana.(none)>
2013-08-05 17:57:45 -0600

    add functionality to config parsing

--- tests/read_config.m
+++ tests/read_config.m
@@ -22,12 +22,15 @@ function config = read_config(config_file)
     
     %search for values
     
+    found = zeros(size(raw_config, 1), 1);
+    
     i = find(strcmp(raw_config, 'folders'));
     if isempty(i)
-        error('read_config:folders', 'No input folder specified (add the line "folders = path1, path2, ... , pathn" to the configuration file');
+        error('read_config:folders', 'No input folder specified (add the line "folders = path1, path2, ... , pathn" to the configuration file)');
     else
         config.folders = raw_config{i,2};
         config.folders = strsplit(config.folders,',');
+        found(i) = 1;
     end
     
     i = find(strcmp(raw_config, 'framerate'));
@@ -36,45 +39,80 @@ function config = read_config(config_file)
         warning('read_config:framerate','Framerate unspecified: defaulting to 24 fps');
     else
         config.framerate = str2double(raw_config{i,2});
+        if isnan(config.framerate)
+            warning('read_config:framerate','Framerate invalid: defaulting to 24 fps');
+        else
+            found(i) = 1;
+        end
     end
     
     i = find(strcmp(raw_config, 'x_scale'));
     if isempty(i)
         config.x_scale = 1;
-        warning('read_config:x_scale','X scale unspecified: defaulting to 1 um/pix');
+        warning('read_config:x_scale','X scale unspecified: defaulting to 1 um/pixel');
     else
         config.x_scale = str2double(raw_config{i,2});
+        if isnan(config.x_scale)
+            warning('read_config:x_scale','X scale invalid: defaulting to 1 um/pixel');
+        else
+            found(i) = 1;
+        end
     end
     
     i = find(strcmp(raw_config, 'y_scale'));
     if isempty(i)
         config.y_scale = 1;
-        warning('read_config:x_scale','Y scale unspecified: defaulting to 1 um/pix');
+        warning('read_config:y_scale','Y scale unspecified: defaulting to 1 um/pixel');
     else
         config.y_scale = str2double(raw_config{i,2});
+        if isnan(config.y_scale)
+            warning('read_config:y_scale','Y scale invalid: defaulting to 1 um/pixel');
+        else
+            found(i) = 1;
+        end
     end
     
     i = find(strcmp(raw_config, 'frame_col'));
     if isempty(i)
         config.frame_col = 1;
-        warning('read_config:x_scale','Column for frame data unspecified: defaulting to column 1');
+        warning('read_config:frame_col','Column for frame data unspecified: defaulting to column 1');
     else
         config.frame_col = str2double(raw_config{i,2});
+        if isnan(config.frame_col)
+            warning('read_config:frame_col','Column for frame data invalid: defaulting to column 1');
+        else
+            found(i) = 1;
+        end
     end
     
     i = find(strcmp(raw_config, 'x_col'));
     if isempty(i)
         config.x_col = 2;
-        warning('read_config:x_scale','Column for x position data unspecified: defaulting to column 2');
+        warning('read_config:x_col','Column for x position data unspecified: defaulting to column 2');
     else
         config.x_col = str2double(raw_config{i,2});
+        if isnan(config.x_col)
+            warning('read_config:x_col','Column for x position data invalid: defaulting to column 2');
+        else
+            found(i) = 1;
+        end
     end
     
     i = find(strcmp(raw_config, 'y_col'));
     if isempty(i)
         config.y_col = 3;
-        warning('read_config:x_scale','Column for y position data unspecified: defaulting to column 3');
+        warning('read_config:y_col','Column for y position data unspecified: defaulting to column 3');
     else
         config.y_col = str2double(raw_config{i,2});
+        if isnan(config.framerate)
+            warning('read_config:y_col','Column for y position data invalid: defaulting to column 3');
+        ene
+            found(i) = 1;
+        end
+    end
+    
+    for i = find(~found)
+        msg = sprintf('Unrecognized option %s. Ignoring.', raw_config{i,1});
+        warning('read_config:unrecognized',msg);
     end
 end
